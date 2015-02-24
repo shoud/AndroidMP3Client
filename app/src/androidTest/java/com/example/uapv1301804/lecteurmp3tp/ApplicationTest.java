b85823dd2dd77eedbde426bcb125788c435b2cab
shoud <shoud@hotmail.fr>
2015-02-24 15:55:55 +0100

    Premier commit

---
+++ app/src/androidTest/java/com/example/uapv1301804/lecteurmp3tp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.uapv1301804.lecteurmp3tp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
